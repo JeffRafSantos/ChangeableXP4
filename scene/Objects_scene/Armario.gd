118a53ce05dadf3625bb88fe487290258e056a8a
Zherst <renatoguan10@gmail.com>
2024-11-10 23:33:31 -0300

    Update world.tscn

--- scene/Objects_scene/Armario.gd
+++ scene/Objects_scene/Armario.gd
@@ -3,6 +3,8 @@ extends Node3D
 signal Open(body)
 
 @onready var timer = $Timer
+@onready var audio_player = $AudioStreamPlayer
+
 @export var prompt_message = "Interact"
 @export var prompt_action = "interact"
 
@@ -42,6 +44,7 @@ func Armario_porta():
 		elif open == false:
 			open = true
 			playback.travel("Animation_Open")
+		audio_player.play()
 		timer.start()
 
 
